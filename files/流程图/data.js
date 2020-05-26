5fcb4c7bce5a70978c34b71563fcbad0b482e790
guky <18201435293@163.com>
2020-05-26 17:35:01 +0800

    给婷婷的一版
    招聘系统完整

---
+++ files/流程图/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(w,bf,bg,_(bh,bi,bj,bk)),S,_(),bl,_(),bm,_(bn,bo))])),bp,_(),bq,_(br,_(bs,bt)));}; 
+var b="url",c="流程图.html",d="generationDate",e=new Date(1590485632113),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="1689cd014112411da5117ef7f43e5f4e",q="type",r="Axure:Page",s="name",t="流程图",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFD7D7D7,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="ebe9fb4dbab44d27b5a2b799b3a0a8bd",Y="label",Z="friendlyType",ba="图片",bb="imageBox",bc="styleType",bd="visible",be=true,bf="5f65f5ffdb164239ada2ea9114cda7c2",bg="size",bh="width",bi=1500,bj="height",bk=2745,bl="imageOverrides",bm="images",bn="normal~",bo="images/流程图/u5885.png",bp="masters",bq="objectPaths",br="ebe9fb4dbab44d27b5a2b799b3a0a8bd",bs="scriptId",bt="u5885";
+return _creator();
+})());
